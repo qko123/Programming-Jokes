7fd557af3a0b2ba36557dec172d89081085d2091
Quinn Kozak <quinn.kozak@icloud.com>
2023-02-07 09:29:43 -0600

    Initial Commit

---
+++ Programming Jokes/Programming_JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Programming_JokesApp.swift
+//  Programming Jokes
+//
+//  Created by Quinn Kozak on 2/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Programming_JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Programming JokesUITests/Programming_JokesUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Programming_JokesUITestsLaunchTests.swift
+//  Programming JokesUITests
+//
+//  Created by Quinn Kozak on 2/7/23.
+//
+
+import XCTest
+
+final class Programming_JokesUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
