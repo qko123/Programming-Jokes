25146577c1c9f57d6aa68248a5751d647e9f47cd
Quinn Kozak <quinn.kozak@icloud.com>
2023-02-07 09:40:28 -0600

    set up content view

--- Programming Jokes/ContentView.swift
+++ Programming Jokes/ContentView.swift
@@ -8,19 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var jokes = [Joke]()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List(jokes) { joke in
+                NavigationLink(destination: Text(joke.punchline)
+                    .padding()) {
+                        Text(joke.setup)
+                    }
+            }
+            .navigationTitle("Programming Jokes")
         }
-        .padding()
+        .onAppear(perform: {
+            getJokes()
+        })
+        
     }
+    func getJokes() {
+
+            jokes.append(Joke(setup: "Why do programmers always get Christmas and Halloween mixed up?",
+
+                              punchline: "Because DEC 25 = OCT 31"))
+
+            jokes.append(Joke(setup: "How did the programmer die in the shower?",
+
+                              punchline: "She followed the shampoo bottle instructions: Lather. Rinse. Repeat."))
+
+            jokes.append(Joke(setup: "There are 10 types of people in the world",
+
+                              punchline: "Those who understand binary and those who don’t."))
+
+        }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+struct Joke: Identifiable {
+let id = UUID()
+    var setup = ""
+    var punchline = ""
 }
